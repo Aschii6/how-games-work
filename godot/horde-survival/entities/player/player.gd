f45e8d72ebb1ac1939b988bdb3249f498d7f96af
Aschii6 <danutlechea2003@yahoo.com>
2026-08-23 20:49:15 +0300

    G - Acceleration & Deceleration

--- godot/horde-survival/entities/player/player.gd
+++ godot/horde-survival/entities/player/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+const ACCELERATION = SPEED * 3
 
 func _physics_process(delta: float) -> void:
 	var input_direction := Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * SPEED
+	var target_velocity = input_direction * SPEED
+	velocity = velocity.move_toward(target_velocity, ACCELERATION * delta)
 	move_and_slide()
